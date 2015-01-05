2ffc6bab7fc2a9ce864034d54afd827c3c550927
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2015-01-05 18:34:00 +1100

    Test with mod_wsgi 4.4.5 before release from setup.py.

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup
 packages = [
     'psycopg2',
     'kallithea',
-    'mod_wsgi>=4.4.4'
+    'mod_wsgi>=4.4.5'
 ]
 
-setup(name='Application', version='1.0', install_requires=packages)
+links = [
+    'https://dl.dropboxusercontent.com/u/22571016/mod_wsgi-4.4.5.tar.gz#egg=mod_wsgi-4.4.5',
+]
+
+setup(name='Application', version='1.0',
+        install_requires=packages, dependency_links=links)
