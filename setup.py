5bce10be7241b771d36872bd9a391cbfe5a2c0c3
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2015-01-05 15:12:37 +1100

    Allow mod_wsgi version higher than 4.4.4.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 packages = [
     'psycopg2',
     'kallithea',
-    'mod_wsgi==4.4.4'
+    'mod_wsgi>=4.4.4'
 ]
 
 setup(name='Application', version='1.0', install_requires=packages)
