f698abebd19d3f66590277bb35009a2de5f58c90
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2015-01-06 08:30:37 +1100

    Move comments on options into app.sh.

--- app.sh
+++ app.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Because average response time for requests against Kallithea are going
+# to be quite long, we increase the capacity for concurrent requests.
+# Each Kallithea instance is quite memory hungry though so although
+# increasing processes over threads can be better, we can't set it too
+# high else we will run out of memory with a small gear. Most requests
+# will be I/O bound on accessing database or waiting on git commands, we
+# use of many threads in one process shouldn't be an issue.
+
 KALLITHEA_HOMEDIR=$OPENSHIFT_DATA_DIR/kallithea-runtime
 KALLITHEA_INIFILE=$KALLITHEA_HOMEDIR/production.ini
 
