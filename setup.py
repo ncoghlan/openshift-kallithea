4a0627802411f84809d15c22ade199c9aa877f61
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2015-01-06 15:35:36 +1100

    Use mod_wsgi from PyPi now that propagated properly.

--- setup.py
+++ setup.py
@@ -6,9 +6,4 @@ packages = [
     'mod_wsgi>=4.4.5'
 ]
 
-links = [
-    'https://dl.dropboxusercontent.com/u/22571016/mod_wsgi-4.4.5.tar.gz#egg=mod_wsgi-4.4.5',
-]
-
-setup(name='Application', version='1.0',
-        install_requires=packages, dependency_links=links)
+setup(name='Application', version='1.0', install_requires=packages)
