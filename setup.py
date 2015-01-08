5ca3faba71c65c661dd244200773e35724d0390a
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2015-01-08 21:18:41 +1100

    Add descriptive documentation on how WSGI server gets started.

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
+'''Use of a 'requirements.txt' file and relying on OpenShift to execute
+the 'pip' command currently has some issues due to 'pip' failing to
+run in some cases when the HOME directory isn't writable, as is the case
+with OpenShift. We therefore use a 'setup.py' to install package
+dependencies as OpenShift will also run 'python setup.py develop' when a
+'setup.py' file exists.
+
+'''
+
 from setuptools import setup
 
 packages = [
